966af44cc3c0145b77e191cc809219dd455751df
tmccombs2019 <tracey.mccombs@verizon.net>
2019-01-21 00:17:49 -0500

    Add project files.

---
+++ HelloWorldv2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
